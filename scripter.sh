a0a622b56f6dad58be9d3d080d129e5f1566c31e
pocketmine-0-11 <williamsmidgered@gmail.com>
2024-08-09 03:19:54 -0400

    scripter file
    lazy workaround hack

---
+++ scripter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true; do
+
+read -p "Do you want to proceed? (y/n) " yn
+
+case $yn in 
+	[yY] ) echo ok, we will proceed;
+		break;;
+	[nN] ) echo exiting...;
+		exit;;
+	* ) echo invalid response;;
+esac
+
+done
+
+bash /pocketmine/server/start.sh
